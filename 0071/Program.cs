120fa10f4dcd79a10a55a6064d358938131025bc
Alexei <datlife2015@yandex.ru>
2022-11-10 12:50:38 +0300

    Задали двумерный массив следующим правилом: Amn = m+n

--- 0071/Program.cs
+++ 0071/Program.cs
@@ -4,32 +4,64 @@ void FillMatrix(double[,] matrix)
     Random rnd = new Random();
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
-    for (int j = 0; j < matrix.GetLength(1); j++)
-        matrix[i, j] = rnd.NextDouble() * 100;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = rnd.NextDouble() * 100;
     }
-}    
-void PrintMatrix(double[,] matrix)
+}
+void PrintMatrixDouble(double[,] matrix)
 {
-     for (int i = 0; i < matrix.GetLength(0); i++)
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
         Console.Write("[");
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            if(j<matrix.GetLength(1)-1)Console.Write($"{matrix[i, j]}; ");
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j]}; ");
+            else Console.Write($"{matrix[i, j]}");
+        }
+        Console.WriteLine("]");
+    }
+}
+// Задать двумерный массив следующим правилом: Amn = m+n
+int[,] CreateMatrixAnm(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i + j;
+        }
+    }
+    return matrix;
+}
+void PrintMatrixAnm(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j]}, ");
             else Console.Write($"{matrix[i, j]}");
         }
         Console.WriteLine("]");
     }
 }
 
-       
+Console.WriteLine("Показать двумерный массив размером m x n заполненный вещественными числами");
 Console.WriteLine("Количество строк: ");
 int m = int.Parse(Console.ReadLine() ?? "0");
 Console.WriteLine("Количество столбцов: ");
 int n = int.Parse(Console.ReadLine() ?? "0");
 
-
 double[,] matrixD = new double[m, n];
 FillMatrix(matrixD);
 Console.WriteLine();
-PrintMatrix(matrixD);
+PrintMatrixDouble(matrixD);
+Console.WriteLine();
+
+Console.WriteLine("Задать двумерный массив следующим правилом: Amn = m+n");
+int[,]matrixAnm=CreateMatrixAnm(4, 4 );
+PrintMatrixAnm(matrixAnm);
+Console.WriteLine();
